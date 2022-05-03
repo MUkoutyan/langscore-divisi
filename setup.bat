97cc2838069dbf32ebe791ddce385945abe857f0
MUkoutyan <koutyan.1923@gmail.com>
2022-05-03 23:38:20 +0900

    依存ライブラリのクローン処理を追加

--- setup.bat
+++ setup.bat
@@ -1,3 +1,8 @@
 git clone https://github.com/nlohmann/json.git
+
+git clone https://github.com/DICE2000/rvdata2json.git
+
+git clone https://github.com/treeder/jsonable ./rvdata2json/jsonable
+
 cd ./json
 cmake 
