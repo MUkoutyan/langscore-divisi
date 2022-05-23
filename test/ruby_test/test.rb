a632301c4da53963688b4ea2cb59d7126d51b6e0
MUkoutyan <koutyan.1923@gmail.com>
2022-05-23 17:51:07 +0900

    ""括りの変更に対応,

--- test/ruby_test/test.rb
+++ test/ruby_test/test.rb
@@ -25,14 +25,15 @@ class TestSample < Test::Unit::TestCase
   end
 
   def test_csvrow1
-
+  
+	write_csv(["original,ja,en,zh", "ほに,ほげ,Honi,Honnni"])
 	lines = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
 
 	assert_equal "original,ja,en,zh\n", lines[0]
 	assert_equal "ほに,ほげ,Honi,Honnni\n", lines[1]
   end
   
-  def test_csvrow1
+  def test_csvrow2
   
 	write_csv(["original,ja,en,zh", "ほに,ほげ,Honi,Honnni", "\n"])
 	lines = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
@@ -43,18 +44,18 @@ class TestSample < Test::Unit::TestCase
   
   def test_csvrow_dq1
 
-	write_csv(["original,ja,en,\"zh", "ほに\",ほげ,Honi,Honnni"])
+	write_csv(["original,ja,en,\"zh", "ほに\",ほげ,\"Honi\",Honnni"])
 	rows = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
+	p rows
 	lines = LSCSV.parse_col(rows)
 
-	assert_equal ["original","ja","en","\"zh\nほに\"","ほげ","Honi","Honnni"], lines[0]
+	assert_equal ["original","ja","en","zh\nほに","ほげ","Honi","Honnni"], lines[0]
   end
 
   def test_csvrow_dq2
 
 	write_csv(["original,ja,en,zh", "\"ほに\"ほに,ほげ,H\"on\"i,Honnni"])
 	rows = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
-	p rows
 	lines = LSCSV.parse_col(rows)
 
 	assert_equal ["original","ja","en","zh"], lines[0]
@@ -66,7 +67,6 @@ class TestSample < Test::Unit::TestCase
 
 	write_csv(["original,ja,en,zh", "\"ほに\"ほに,ほげ,H\"on\"i,"])
 	rows = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
-	p rows
 	lines = LSCSV.parse_col(rows)
 
 	LSCSV.varidate("test_csv.csv", lines)
