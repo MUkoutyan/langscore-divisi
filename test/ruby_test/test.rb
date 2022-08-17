a54fe7427c4b8847d1244d507e1d810e25249663
MUkoutyan <koutyan.1923@gmail.com>
2022-08-18 00:57:24 +0900

    テストを追加(テスト用ゲームプロジェクトは未追加)

--- test/ruby_test/test.rb
+++ test/ruby_test/test.rb
@@ -8,8 +8,8 @@ end
 
 class TestSample < Test::Unit::TestCase
 
-  def write_csv(lines)
-	File.open("test_csv.csv", "w") do | file |
+  def write_csv(lines, filename = "test_csv.csv")
+	File.open(filename, "w") do | file |
 	  lines.each do |l|
         file.puts(l)
 	  end
@@ -46,7 +46,6 @@ class TestSample < Test::Unit::TestCase
 
 	write_csv(["original,ja,en,\"zh", "ほに\",ほげ,\"Honi\",Honnni"])
 	rows = LSCSV.parse_row(LSCSV.open("test_csv.csv"))
-	p rows
 	lines = LSCSV.parse_col(rows)
 
 	assert_equal ["original","ja","en","zh\nほに","ほげ","Honi","Honnni"], lines[0]
@@ -60,6 +59,50 @@ class TestSample < Test::Unit::TestCase
 
 	assert_equal ["original","ja","en","zh"], lines[0]
 	assert_equal ["\"ほに\"ほに","ほげ","H\"on\"i","Honnni"], lines[1]
+  end
+
+  def test_csvrow_dq3
+
+  text = ["\"ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"\"いない\"\"んだろう。」
+\",\"en-ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"\"いない\"\"んだろう。」
+\",\"ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"\"いない\"\"んだろう。」
+\",\"zh-sc-ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"\"いない\"\"んだろう。」
+\""]
+
+	write_csv(text, "test1.csv")
+	rows = LSCSV.parse_row(LSCSV.open("test1.csv"))
+	lines = LSCSV.parse_col(rows)
+
+	result_text = ["ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"いない\"んだろう。」
+","en-ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"いない\"んだろう。」
+","ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"いない\"んだろう。」
+","zh-sc-ゲオルイース
+「魔族には本来【魔王】という絶対的な王が
+　存在するはずなのだが、この七年間一度も姿を見せていない。
+　おそらく・・・本当に\"いない\"んだろう。」
+"]
+	assert_equal result_text, lines[0]
   end
 
   
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,21 +1,161 @@
+﻿#pragma once
 #include <iutest.hpp>
 #include "config.h"
+#include "invoker.h"
+#include "divisi.h"
+#include "..\\src\\reader\\csvreader.h"
+#include "..\\src\\utility.hpp"
+#include <iostream>
+#include <filesystem>
+#include <Windows.h>
 
 using namespace std::string_literals;
+namespace fs = std::filesystem;
 
-IUTEST(Langscore_Config, LoadConfig)
+IUTEST(Langscore_Config, TmpDir)
 {
 	langscore::config config;
+	
+	auto expected = config.langscoreAnalyzeDirectorty();
+	auto actual = fs::path(u8"D:\\Programming\\Github\\langscore-divisi\\out\\build\\Test\\data\\ソポァゼゾタダＡボマミ_langscore\\analyze"s);
+	IUTEST_ASSERT_STREQ(expected, actual.u8string());
+}
 
-	utility::stringlist actual = {"ja", "en", "zh-sc"};
+IUTEST(Langscore_Config, LoadLanguages)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+
 	auto expected = config.languages();
+	utility::stringlist actual = {"en", "ja"};
 	for(int i = 0; i < actual.size(); ++i){
-		IUTEST_ASSERT_EQ(expected[i], actual[i]);
+		IUTEST_ASSERT_STREQ(expected[i].name, actual[i]);
 	}
+	actual = {"es", "zh-tw"};
+	for(int i = 0; i < actual.size(); ++i){
+		IUTEST_ASSERT_STRNE(expected[i].name, actual[i]);
+	}
+}
+
+IUTEST(Langscore_Config, CheckFontName)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+
+	auto expected = config.languages();
+	utility::u8stringlist actual = {u8"VL Gothic"s, u8"メイリオ"s};
+	for(int i = 0; i < actual.size(); ++i)
+	{
+		IUTEST_ASSERT_STREQ(expected[i].font.name, actual[i]);
+	}
+}
+
+IUTEST(Langscore_Config, CheckProjectPath)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+
+	auto expected = config.projectPath();
+	auto actual = fs::path(u8"D:\\Programming\\Github\\langscore-divisi\\out\\build\\Test\\data\\ソポァゼゾタダＡボマミ"s);
+	IUTEST_ASSERT_STREQ(expected, actual.u8string());
+}
+
+IUTEST(Langscore_Csv, read1)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+
+	utility::stringlist actual = {"en", "ja"};
+	auto expected = config.languages();
+	for(int i = 0; i < actual.size(); ++i)
+	{
+		IUTEST_ASSERT_STREQ(expected[i].name, actual[i]);
+	}
+}
+
+IUTEST(Langscore_Invoker, NoAssignProject)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+	langscore::invoker invoker;
+	auto result = invoker.analyze();
+	IUTEST_ASSERT_EQ(result.val(), 1);
+}
+
+IUTEST(Langscore_Invoker, AnalyzeVXAceProject)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+	auto outputPath = fs::path(config.langscoreAnalyzeDirectorty());
+	std::filesystem::remove_all(outputPath);
+	langscore::invoker invoker;
+	invoker.setProjectPath(langscore::invoker::VXAce, config.projectPath());
+	auto analyzeResult = invoker.analyze();
+	IUTEST_ASSERT_EQ(analyzeResult.val(), 0);
+
+	auto itr = fs::recursive_directory_iterator(outputPath);
+	auto numFiles = std::distance(itr, fs::recursive_directory_iterator{});
+	IUTEST_ASSERT_LT(135, numFiles);
+
+	fs::remove_all(outputPath);
+}
+
+IUTEST(Langscore_Invoker, CheckValidScriptList)
+{
+	langscore::config config(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+	auto outputPath = fs::path(config.langscoreAnalyzeDirectorty());
+	std::filesystem::remove_all(outputPath);
+	langscore::invoker invoker;
+	invoker.setProjectPath(langscore::invoker::VXAce, config.projectPath());
+	invoker.analyze();
+
+	IUTEST_ASSERT(fs::exists(outputPath / "Scripts/_list.csv"));
+	langscore::csvreader csvreader;
+	auto scriptList = csvreader.parsePlain(outputPath / "Scripts/_list.csv");
+	
+	for(auto& file : fs::recursive_directory_iterator(outputPath/"Scripts"))
+	{
+		auto filename = file.path().filename();
+		if(filename.extension() != ".rb"){ continue; }
+		filename = filename.stem();
+		auto r = std::find_if(scriptList.cbegin(), scriptList.cend(), [&filename](const auto& x){
+			return x[0] == filename;
+		});
+		if(r == scriptList.cend()){ 
+			std::cout << "Not Found!" << filename << std::endl;
+			IUTEST_FAIL(); 
+		}
+	}
+
+	std::filesystem::remove_all(outputPath);
+
+	IUTEST_SUCCEED();
+}
+
+IUTEST(Langscore_Divisi, CheckScriptCSV)
+{	
+	langscore::divisi divisi("./", ".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
+
+	IUTEST_ASSERT(divisi.analyze());
+	langscore::config config;
+	auto outputPath = fs::path(config.langscoreAnalyzeDirectorty());
+
+	IUTEST_ASSERT(fs::exists(outputPath / "Scripts.csv"));
+	langscore::csvreader csvreader;
+	auto scriptCsv = csvreader.parse(outputPath / "Scripts.csv");
+	auto scriptList = csvreader.parsePlain(outputPath / "Scripts/_list.csv");
+
+	for(auto& obj : scriptCsv)
+	{
+		auto result = std::find_if(scriptList.cbegin(), scriptList.cend(), [&obj](const auto& x){
+			return obj.original.find(x[0]) == std::u8string::npos;
+		});
+		if(result == scriptList.cend()){
+			std::cout << "Not Found!" << std::string((*result)[0].begin(), (*result)[0].end()) << std::endl;
+			IUTEST_FAIL();
+		}
+	}
+
+	IUTEST_SUCCEED();
 }
 
 int main(int argc, char** argv)
 {
+	langscore::config::attachConfigFile(".\\data\\ソポァゼゾタダＡボマミ_langscore\\test_config_with.json");
 	IUTEST_INIT(&argc, argv);
 	return IUTEST_RUN_ALL_TESTS();
 }
