ed73bec3c7e97b3fd001c24b0ab673eb69c504d1
MUkoutyan <koutyan.1923@gmail.com>
2024-08-05 00:52:59 +0900

    テスト
    リソース無しでもテストが実行できるように調整。

--- test/plugin/mv_test/js/plugins/Langscore_test.js
+++ test/plugin/mv_test/js/plugins/Langscore_test.js
@@ -7,6 +7,9 @@ Utils.isNwjs = function()
 Graphics.printError = function(name, message){
     console.log(`Error : ${name} ${message}`);
 }
+Graphics.setLoadingImage = function(){
+    this._loadingImage = new Image();
+}
 
 class LangscoreTestEvent {
 
@@ -14,6 +17,23 @@ class LangscoreTestEvent {
 
 Scene_Boot.prototype.isGameFontLoaded = function() {
     return true;
+};
+
+Scene_Boot.prototype.loadSystemWindowImage = function() {
+};
+Scene_Boot.loadSystemImages = function(){
+}
+
+Scene_Title.prototype.createBackground = function(){
+    this._backSprite1 = new Sprite(ImageManager.loadEmptyBitmap());
+    this._backSprite2 = new Sprite(ImageManager.loadEmptyBitmap());
+    this.addChild(this._backSprite1);
+    this.addChild(this._backSprite2);
+}
+Scene_Title.prototype.createForeground = function(){
+}
+Window_Base.prototype.loadWindowskin = function() {
+    this.windowskin = ImageManager.loadEmptyBitmap();
 };
 
 SceneManager.initAudio = function(){};
