dc730d2bf5d84de81a839fe9e592d3b8c35c2b70
MUkoutyan <koutyan.1923@gmail.com>
2024-08-15 03:29:08 +0900

    rvcnv
    ビルドに使用するRubyのバージョンを、インストール後の追加作業無しで使用できるバージョンに変更。(3.3.4 -> 2.7.2)
    ※ビルドが正しくできない & 出来上がるバイナリが不正になる問題への対処

--- rvcnv/build.ps1
+++ rvcnv/build.ps1
@@ -7,9 +7,10 @@ $RUBY_DIR = Join-Path -Path $PSScriptRoot -ChildPath "ruby"
 # Rubyの実行ファイルパスを設定
 $RUBY_EXE = Join-Path -Path $RUBY_DIR -ChildPath "bin\ruby.exe"
 $GEM_CMD = Join-Path -Path $RUBY_DIR -ChildPath "bin\gem.cmd"
-$RUBY_INSTALLER_URL = "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-3.3.4-1/rubyinstaller-3.3.4-1-x64.7z"
+$RUBY_INSTALL_VERSION = "2.7.2-1"
+$RUBY_INSTALLER_URL = "https://github.com/oneclick/rubyinstaller2/releases/download/RubyInstaller-$RUBY_INSTALL_VERSION/rubyinstaller-$RUBY_INSTALL_VERSION-x64.7z"
 $RUBY_INSTALLER_EXE = Join-Path -Path $PSScriptRoot -ChildPath "rubyinstaller.7z"
-$EXPECTED_HASH = "c3e55de3a1924848a871b4406eff6e78e8d6de06fc59186dcfec6ceb44d635a9"
+$EXPECTED_HASH = "925cc01d453951d1d0c077c44cec90849afc8d23e45946e19ecd3aaabc0c0ab3"
 
 Write-Output "Check Path $RUBY_EXE"
 
@@ -40,7 +41,7 @@ if (Test-Path -Path $RUBY_EXE) {
 
         tar -xzf "$RUBY_INSTALLER_EXE"
 
-        Rename-Item .\rubyinstaller-3.3.4-1-x64 .\ruby
+        Rename-Item ".\rubyinstaller-$RUBY_INSTALL_VERSION-x64" .\ruby
 
         # インストーラを削除
         Remove-Item -Path $RUBY_INSTALLER_EXE
@@ -67,7 +68,7 @@ if ($LASTEXITCODE -ne 0) {
 Write-Output "ocra rvcnv.rb --windows"
 Write-Output "make rvcnv..."
 # --windowは不可。--consoleにすること。
-# ruby 3.3.4-1, ocra最新では libgmp-10.dll, zlib1.dll の追加が必要。
-& $RUBY_DIR\bin\ocra.bat rvcnv.rb --console --gem-all --add-all-core --icon "./icon.ico" --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\zlib1.dll 
+# デバッグする場合は--debug-extractを追加。
+& $RUBY_DIR\bin\ocra.bat rvcnv.rb --console --gem-all --add-all-core --icon "./icon.ico" --dll ruby_builtin_dlls\libssp-0.dll --dll ruby_builtin_dlls\libwinpthread-1.dll --dll ruby_builtin_dlls\libgmp-10.dll --dll ruby_builtin_dlls\zlib1.dll 
 Write-Output "done."
 & $RUBY_EXE generate_hash.rb
