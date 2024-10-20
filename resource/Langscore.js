5d025ed58a9f556bf17c6b8bc39d31964d98ab98
MUkoutyan <koutyan.1923@gmail.com>
2024-10-20 16:59:38 +0900

    [バージョン更新][プラグイン]
    プラグインバージョンをVer.0.9.10に変更

--- resource/Langscore.js
+++ resource/Langscore.js
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------
 // 
 // Langscore CoreScript "Unison" 
-// Version 0.9.9
+// Version 0.9.10
 // Written by BreezeSinfonia 來奈津
 // 
 // 注意：このスクリプトは自動生成されました。編集は非推奨です。
